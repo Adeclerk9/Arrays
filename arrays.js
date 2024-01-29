cf0ec0e19032425d7a248410329d0d197dd317e2
Adeclerk9 <ang_declerk@yahoo.com>
2024-01-28 21:43:14 -0600

    Arrays

---
+++ arrays.js
@@ -0,0 +1,30 @@
+var arr = new Array ();
+
+arr[0] = "Candy Land";
+arr[1] = "Sorry";
+arr[2] = "Uno";
+arr[3] = "Battleship";
+
+console.log(arr);
+
+function turns(number) {
+    console.log("How many turns have there been? " + number);
+}
+
+turns("1");
+
+function fuel(price, amount) {
+    console.log("Total amount is " (price * amount));
+}
+
+const price = 1.00;
+const amount = 20.00;
+
+var arr = {};
+
+console.log(arr);
+
+for (let turns in arr) {
+    console.log(fuel);
+    console.log(turns);
+}
